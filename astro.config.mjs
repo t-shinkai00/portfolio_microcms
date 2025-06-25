e6d01c4e4a7271d89fb7bfab93f1ef6fbe53a9f7
t-shinkai00 <t.shinkai00@gmail.com>
2025-06-25 14:52:07 +0900

    Add Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://astro-simple-blog-omega.vercel.app/",
   integrations: [tailwind()],
+  adapter: vercel(),
 });
